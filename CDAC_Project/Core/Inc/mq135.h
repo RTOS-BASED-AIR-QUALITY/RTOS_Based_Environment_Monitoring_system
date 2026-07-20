37e4d41639d956b061d3ab2d6f217233fe4948f5
Muhammad Karim shaikh <karimshaikh111324@gmail.com>
2026-07-20 11:30:00 +0530

    feat(mq135): implement ADC sampling and averaging

--- CDAC_Project/Core/Inc/mq135.h
+++ CDAC_Project/Core/Inc/mq135.h
@@ -8,5 +8,14 @@
 #ifndef INC_MQ135_H_
 #define INC_MQ135_H_
 
+#include "main.h"
+
+typedef struct MQ135_Data
+{
+	 uint16_t ADC_val;
+	 float sensorResistace_ratio;
+} MQ135_Data;
+
+MQ135_Data ADCRead();
 
 #endif /* INC_MQ135_H_ */
---
+++ CDAC_Project/Core/Src/mq135.c
@@ -0,0 +1,35 @@
+/*
+ * ADC_read.c
+ *
+ *  Created on: 05-Jul-2026
+ *      Author: Dhiraj
+ */
+
+#include <mq135.h>
+extern ADC_HandleTypeDef hadc1;
+
+MQ135_Data ADCRead()
+{
+	uint32_t ADC_sum = 0, i;
+	MQ135_Data data;
+
+
+	for(i = 0; i<5; i++)
+	{
+		HAL_ADC_Start(&hadc1);
+
+		HAL_ADC_PollForConversion(&hadc1, 10);
+
+		ADC_sum += HAL_ADC_GetValue(&hadc1);
+
+		HAL_ADC_Stop(&hadc1);
+	}
+
+	data.ADC_val = (uint16_t)ADC_sum/5;
+
+	float RS= MQ135calculateRS(data.ADC_val);
+
+	data.sensorResistace_ratio = (RS / R0);
+
+	return data;
+}
