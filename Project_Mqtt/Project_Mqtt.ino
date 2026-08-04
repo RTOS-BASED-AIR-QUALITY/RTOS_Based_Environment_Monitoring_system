690ee8aee3fb6524a2e3d126947e00a79846bc7c
MD Akif Amaan <akifamaan7@gmail.com>
2026-08-04 11:00:00 +0530

    feat(esp32): add WiFi connectivity and MQTT publisher

--- Project_Mqtt/Project_Mqtt.ino
+++ Project_Mqtt/Project_Mqtt.ino
@@ -8,6 +8,16 @@ HardwareSerial StmSerial(2);
 const int RX_PIN = 16;
 const int TX_PIN = 17;
 
+const char *ssid = "POCOF5";
+const char *password = "1234567890";
+
+const char *host = "10.163.98.34";
+const int port = 1883;
+const char *topic = "airsense/data";
+
+WiFiClient wifiClient;
+MqttClient publisher(wifiClient);
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -16,7 +26,27 @@ void setup() {
   Serial.begin(115200);
 
   // Custom Uart to recieve from STM32
-  // StmSerial.begin(115200, SERIAL_8N1, RX_PIN, TX_PIN);
+  StmSerial.begin(115200, SERIAL_8N1, RX_PIN, TX_PIN);
+  
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  Serial.print("Connecting to WiFi");
+  while(WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.print("\nConnected to WiFi");
+  Serial.print("IP Address : ");
+  Serial.println(WiFi.localIP());
+
+  if(!publisher.connect(host, port))
+  {
+    Serial.print("MQTT connection failed: ");
+    Serial.println(publisher.connectError());
+  }
+  Serial.println("Connected to the broker !!!");
 
  
 }
@@ -28,7 +58,9 @@ void loop() {
   while (StmSerial.available())
   {
     String data = StmSerial.readStringUntil('\n');
-    Serial.println(data);
+    publisher.beginMessage(topic);
+    publisher.print(data);
+    publisher .endMessage();
   }
 
   // JSON Data 
